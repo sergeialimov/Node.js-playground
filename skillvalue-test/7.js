63d0c7bd06cbec9d4660bd38a563f35784688cfe
sergeialimov <8912652@gmail.com>
2022-04-27 07:14:56 +0300

    add extra task for promises

---
+++ skillvalue-test/7.js
@@ -0,0 +1,23 @@
+/**
+ * What is the output of the following code?
+ * 
+ * The answer: catch 1 smth
+ * 
+ * Comments: 
+ * reject here is the single parameter placed first. In reality that's not rejece, but resolve
+ */
+
+function methodToReject(param) {
+  return new Promise((reject) => { // resolve
+    reject(Promise.reject('smth'));
+  });
+}
+
+methodToReject()
+  .catch((err) => {
+    console.error(`catch 1 ${err}`);
+  })
+  .catch((err) => {
+    console.error(`catch 2 ${err}`);
+  })
+
