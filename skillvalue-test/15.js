336c40b79c5400c7435cb29a7550624c45f6be59
sergeialimov <8912652@gmail.com>
2022-04-27 17:15:06 +0300

    update the naming

---
+++ skillvalue-test/15.js
@@ -0,0 +1,15 @@
+/**
+ * What's the difference between two exports
+ * 
+ * The answer example is an instance and Example is a class
+ */
+
+class Example {
+  constructor(incomming) {
+    console.log('called');
+    this.param = incomming;
+  }
+}
+
+module.exports.example = new Example;
+module.exports.Example = Example;
