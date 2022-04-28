01c559c9d442844ecae9ef16f29090101a36cdb4
sergeialimov <8912652@gmail.com>
2022-04-28 12:55:29 +0300

    add skillvalue-test/16/run

---
+++ skillvalue-test/16/run.js
@@ -0,0 +1,14 @@
+/**
+ *  How many times will 'called appear?'
+ *
+ * The answer: two times
+ *
+ */
+
+// import { Example, example } from './16.js';
+const { Example, example } = require('./16.js');
+
+const example1 = example;
+const example2 = example;
+const example3 = example;
+const example4 = new Example(4);
