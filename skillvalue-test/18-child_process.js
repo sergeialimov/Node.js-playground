a713e738116951f95b807cf98114d8480c844dd5
sergeialimov <8912652@gmail.com>
2022-04-28 13:27:42 +0300

    add skillvalue-test/18-child_process.js

--- skillvalue-test/18-child_process.js
+++ skillvalue-test/18-child_process.js
@@ -3,9 +3,16 @@
  * from child_process
  * 
  * 
- * 
- * The answer: 
+ * The answer: A communication channel is established to the child
+ *   when using fork
  * 
  * 
  * Comments: 
+ * 
+ * spawn - spawns the child process asynchronously,
+ *   without blocking the Node.js event loop
+ * 
+ * fork - spawns a new Node.js process and invokes a specified module
+ *   with an IPC communication channel established that allows
+ *   sending messages between parent and child.
  */
