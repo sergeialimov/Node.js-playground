e579d42e4d22b3deef2243a77df55f0495bb531f
sergeialimov <8912652@gmail.com>
2022-05-03 19:27:47 +0300

    add parseline

--- parse-csv/parseline.js
+++ parse-csv/parseline.js
@@ -1,5 +1,13 @@
+// function parseline(line) {
+//   const data = line.split('\t');
+//   if (data[1] && data[1].match(/^Paris$/g))
+//     counter++;
+// }
 function parseline(line) {
-  const data = line.split('\t');
-  if (data[1] && data[1].match(/^Paris$/g))
-    counter++;
+	const data = line.split('\t');
+  let counter = 0;
+	if (data[1] && data[1].match(/^Paris$/g))
+		counter++;
 }
+
+module.exports.parseline = parseline;
