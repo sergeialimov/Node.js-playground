ab039e52a3e2f9d18da7313712d1391d0f398db8
sergeialimov <8912652@gmail.com>
2022-05-03 19:27:35 +0300

    add 2020-solution

--- parse-csv/2020-solution.js
+++ parse-csv/2020-solution.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 
 const readline = require('readline');
-const parseline = require('parsline');
+const { parseline } = require('./parseline');
 
-const rs = fs.createReadStream()
+const rs = fs.createReadStream('allCountries.txt');
+const rl = readline.createInterface({ input: rs });
+
+(async () => {
+  console.time(__filename);
+  for await (const line of rl) {
+    parseline(line);
+  }
+  console.timeEnd(__filename);
+})();
