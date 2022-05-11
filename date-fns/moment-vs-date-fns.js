59b847ea3460530ebc0e5317b60323332ba84422
sergeialimov <8912652@gmail.com>
2022-05-11 16:34:19 +0300

    add utc time example

--- date-fns/moment-vs-date-fns.js
+++ date-fns/moment-vs-date-fns.js
@@ -1,8 +1,15 @@
+// moment
 import moment from 'moment';
-// import { }
 
 const momentDate = moment.utc();
-console.log('moment utc', momentDate); // Moment<2022-05-06T14:41:06Z>
+console.log('utc 1', momentDate); // Moment<2022-05-06T14:41:06Z>
+
+const timestamp = moment.utc().toDate();
+console.log('utc 2', timestamp);
 
 
+// date-fns
+import { format, utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz/esm';
 
+const zoned = zonedTimeToUtc(new Date());
+console.log('utc 3', zoned);
